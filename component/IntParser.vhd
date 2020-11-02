09ef5a9039507c41d832e6ccea6c6eab61831494
Ákos Hadnagy <akos.hadnagy@gmail.com>
2020-11-02 20:22:36 +0100

    IntParser corner-case fix

--- component/IntParser.vhd
+++ component/IntParser.vhd
@@ -177,8 +177,10 @@ architecture behavioral of IntParser is
     
           -- Clear output holding register if transfer was accepted.
           if to_x01(dd_in_ready) = '1' then
+            if dd_in_valid = '1' then
+              dd_in := dd_stage_t_init;
+            end if;
             dd_in_valid := '0';
-            dd_in := dd_stage_t_init;
           end if;
 
           
